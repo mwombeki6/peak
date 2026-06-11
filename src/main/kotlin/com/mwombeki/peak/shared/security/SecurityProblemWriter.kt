474b4eccffdda04699ae0ce07fdd389289c0b662
mwombeki6 <hackerlubex@protonmail.com>
2026-06-12 00:21:16 +0300

    Add HTTP security foundation

---
+++ src/main/kotlin/com/mwombeki/peak/shared/security/SecurityProblemWriter.kt
@@ -0,0 +1,39 @@
+package com.mwombeki.peak.shared.security
+
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+
+@Component
+class SecurityProblemWriter {
+    fun write(
+        response: HttpServletResponse,
+        status: HttpStatus,
+        title: String,
+        detail: String,
+    ) {
+        response.status = status.value()
+        response.contentType = MediaType.APPLICATION_PROBLEM_JSON_VALUE
+        response.writer.write(
+            """
+            {"type":"about:blank","title":"${title.jsonEscaped()}","status":${status.value()},"detail":"${detail.jsonEscaped()}"}
+            """.trimIndent(),
+        )
+    }
+
+    private fun String.jsonEscaped(): String {
+        return buildString {
+            for (char in this@jsonEscaped) {
+                when (char) {
+                    '\\' -> append("\\\\")
+                    '"' -> append("\\\"")
+                    '\n' -> append("\\n")
+                    '\r' -> append("\\r")
+                    '\t' -> append("\\t")
+                    else -> append(char)
+                }
+            }
+        }
+    }
+}
